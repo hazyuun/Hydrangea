cc3257d2ad328e41cf36dc6270da6efe23ce2d11
A-Rain-Lover <younessgeek55@gmail.com>
2021-04-08 00:23:05 +0100

    Added: dynamic list data structure

---
+++ kernel/ds/list.h
@@ -0,0 +1,17 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdint.h>
+
+typedef struct {
+  void **data;
+  uint32_t used;
+  uint32_t size;
+} list_t;
+
+list_t *make_list(uint32_t size);
+void list_init(list_t *list, uint32_t size);
+void list_push(list_t *list, void *element);
+uint8_t list_delete(list_t *list, uint32_t index);
+void *list_get(list_t *list, uint32_t index);
+
+#endif
