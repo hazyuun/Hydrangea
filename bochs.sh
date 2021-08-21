94e9c2e1d92d5fa22d81a66b262c8a315de0f7c3
A-Rain-Lover <younessgeek55@gmail.com>
2021-08-22 00:01:33 +0100

    Bash scripts to quickly run in qemu and bochs

---
+++ bochs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ISO=./yuunos.iso
+
+make iso
+
+bochs
---
+++ qemu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ISO=./yuunos.iso
+
+make iso
+
+case "$1" in
+  "--gdb")
+    echo "[*] Running qemu in gdb debug mode"
+    qemu-system-i386 -cdrom $ISO -serial stdio -s -S
+  ;;
+
+  *)
+    qemu-system-i386 -cdrom $ISO -serial stdio
+  ;;
+
+esac
